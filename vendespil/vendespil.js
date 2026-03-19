2a3b93f951ae0bd0a29ec1ca1df39d50b9e73ec3
jessicakangas <jessica6990@icloud.com>
2026-03-19 14:43:00 +0100

    Justerer lydstyrke for baggrundsmusik og krabbelyd

--- vendespil/vendespil.js
+++ vendespil/vendespil.js
@@ -5,7 +5,7 @@ const kallekrabbeLyd = new Audio('sounds/kallekrabbelyd.m4a');
 kallekrabbeLyd.volume = 1.0;
 
 // Tilføjer baggrundsmusik til vendespillet//
-//Justerer lydstyrke for baggrundsmusik og krabbelyd//
+//Justerer lydstyrke for baggrundsmusik og krabbelyd //
 const baggrundsLyd = new Audio('sounds/baggrundslyd.m4a');
 baggrundsLyd.volume = 0.3;
 baggrundsLyd.loop = true; 
