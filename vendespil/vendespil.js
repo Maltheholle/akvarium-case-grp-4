7a9c37d53b790a134126497ec75fe221b0bbde06
jessicakangas <jessica6990@icloud.com>
2026-03-19 14:42:42 +0100

    Tilføjer baggrundsmusik til vendespillet

--- vendespil/vendespil.js
+++ vendespil/vendespil.js
@@ -3,3 +3,9 @@
 //Tilføjer lyd til Kalle Krabbe ved klik//
 const kallekrabbeLyd = new Audio('sounds/kallekrabbelyd.m4a');
 kallekrabbeLyd.volume = 1.0;
+
+// Tilføjer baggrundsmusik til vendespillet//
+//Justerer lydstyrke for baggrundsmusik og krabbelyd//
+const baggrundsLyd = new Audio('sounds/baggrundslyd.m4a');
+baggrundsLyd.volume = 0.3;
+baggrundsLyd.loop = true; 
