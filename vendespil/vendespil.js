831447e44eeb2437d5d4c1739698c21548eb9403
jessicakangas <jessica6990@icloud.com>
2026-03-19 14:49:44 +0100

    Tilføjer havdyrsbilleder til kortene

--- vendespil/vendespil.js
+++ vendespil/vendespil.js
@@ -9,3 +9,39 @@ kallekrabbeLyd.volume = 1.0;
 const baggrundsLyd = new Audio('sounds/baggrundslyd.m4a');
 baggrundsLyd.volume = 0.3;
 baggrundsLyd.loop = true; 
+
+// Tilføjer havdyrsbilleder til kortene//
+const cardsArray = [
+  {
+    name: 'item01',
+    img: 'vendespil/vendespil.img/dory.png'
+  },
+  {
+    name: 'item02',
+    img: 'vendespil/vendespil.img/haj.png'
+  },
+  {
+    name: 'item03',
+    img: 'vendespil/vendespil.img/havfrue.png'
+  },
+  {
+    name: 'item04',
+    img: 'vendespil/vendespil.img/hval.png'
+  },
+  {
+    name: 'item05',
+    img: 'vendespil/vendespil.img/krabbe.png'
+  },
+  {
+    name: 'item06',
+    img: 'vendespil/vendespil.img/nemo.png'
+  },
+  {
+    name: 'item07',
+    img: 'vendespil/vendespil.img/soehest.png'
+  },
+  {
+    name: 'item08',
+    img: 'vendespil/vendespil.img/soestjerne.png'
+  }
+];
